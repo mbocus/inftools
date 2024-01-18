25655e7ecb1f325b81ec7df6a71c33c59eab6c5d
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-01-18 12:09:07 +0100

    Update bin.py

--- inftools/bin.py
+++ inftools/bin.py
@@ -12,47 +12,55 @@ from inftools.puckering_exercise.puckering import (
 )
 from inftools.wham.wham import wham
 
+# NOTE: when defining new functionality
+# put the import statements in the function defenition
+# as to avoid importing loads of libraries, which slows
+# down the `inft` call from the command line
+
+MAPPER = {
+        "wham":wham,
+        "check_indices":check_indices,
+        "concatenate":concatenate,
+        "generate_openff_topology":generate_openff_topology,
+        "initial_path_from_iretis":initial_path_from_iretis,
+        "initial_path_from_md":initial_path_from_md,
+        "plot_order":plot_order,
+        "recalculate_order":recalculate_order,
+        }
 
 def inftool():
     """Map an inftool command to a python function.
 
-    Usage from the command line
+    Usage from the command line:
         inft `tool_name` `arguments`
-
-    `tool_name` is a the name of the tool you 
-    want to use.
-    `arguments` the arguments passed to the tool.
+    
+    `tool_name`: the name of the tool you  want to use
+    `arguments`: the arguments passed to the tool
 
     To get more information about each tool, pass `-h` after
-    specifying the tool name.
+    specifying the tool name, e.g. 'inft wham -h'.
+
+    Example usage:
+        inft wham -toml infretis.toml -data infretis_data.txt
+        inft concatenate -h
+
     """
+    if len(sys.argv) == 1 or sys.argv[1] in ["-h","help","--help"]:
+        print(inftool.__doc__)
+        print("Available inft commands:")
+        for key in MAPPER.keys():
+            print(f"\t{key}")
+        return
 
     tool_name = sys.argv[1]
     arguments = sys.argv[1:]
     
-    # NOTE: when defining new functionality
-    # put the import statements in the function defenition
-    # as to avoid importing loads of libraries, which slows
-    # down the `inft` call from the command line
-    mapper = {
-            "wham":wham,
-            "check_indices":check_indices,
-            "concatenate":concatenate,
-            "generate_openff_topology":generate_openff_topology,
-            "initial_path_from_iretis":initial_path_from_iretis,
-            "initial_path_from_md":initial_path_from_md,
-            "plot_order":plot_order,
-            "recalculate_order":recalculate_order,
-            }
-
-    if sys.argv[1] in ["-h","help","--help"]:
-        print(inftool.__doc__)
-        print("Available inft commands:")
-        for key in mapper.keys():
-            print(f"\t{key}")
-        return
-
-    tool = mapper[tool_name]
+    if tool_name not in list(MAPPER.keys()):
+        msg = f"No tool named '{tool_name}', maybe you spelled it wrong?\n \
+                \nFor usage information run\n\tinft -h"
+        return msg
+        
+    tool = MAPPER[tool_name]
     # run the tool function
     tool(arguments)
 
