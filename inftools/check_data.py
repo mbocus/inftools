15512a840a304980b55fafbaf29995c6df14330b
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-04-27 18:42:22 +0200

    bug fix

--- inftools/check_data.py
+++ inftools/check_data.py
@@ -54,7 +54,10 @@ def check_data(arguments):
             a[ens].axvline(M, c='k', lw=3)
             a[ens].scatter(op,x[path_nr, 0].astype(int))
             a[ens].set(xlim=(L, max(max(op), M)), xlabel = 'maximum OP', ylabel = 'path nr. in infretis_data.txt', title = f"ensemble: {ens}, intf={M}")
-        if ens > 0 and max(op) < M:
+        #print(ens, op)
+        if len(op)==0:
+            print(f"Ensemble {ens} does not contain any paths.")
+        elif ens > 0 and max(op) < M:
             print(f"Ensemble {ens} is missing paths that cross interface {ens_intf+1} with value {M}")
 
     if args.plot:
