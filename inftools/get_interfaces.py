2c49e43504019c0e1b2afe850c351fcbf489c7e4
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-05-10 09:16:24 +0200

    update est_intf to allow setting the position of first and last interfaces

--- inftools/get_interfaces.py
+++ inftools/get_interfaces.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
 def estimate_interfaces(arguments):
     """
     A simple script to recaluclate the interface positions
@@ -12,6 +9,8 @@ def estimate_interfaces(arguments):
     increasing x, we can play with the hyperparameter <alpha> that
     penalizes derivatives greater than zero.
     """
+    import matplotlib.pyplot as plt
+    import numpy as np
     from scipy.optimize import curve_fit, minimize
     import argparse
 
@@ -20,28 +19,40 @@ def estimate_interfaces(arguments):
     )
     parser.add_argument("-i", help="The Pcross.txt file")
     parser.add_argument("-num", type=int, help="Number of interfaces")
-    parser.add_argument("-order", type=int, required=False, help="Polynomial order")
+    parser.add_argument("-order", type=int, required=False, help="Polynomial order",
+            default = 5)
+    parser.add_argument("-i0", default = "intf0",
+                        help = "Position of the first interface")
+    parser.add_argument("-iN", default = "intfN",
+                        help = "Position of the last interface")
     parser.add_argument("-plot", action="store_true", help="Plot")
 
     args = parser.parse_args(arguments)
 
     x = np.loadtxt(args.i)
-    i0 = x[0, 0]  # set first interface
-    iN = x[-1, 0]  # set last interface
+
+    if args.i0 == "intf0":
+        i0 = x[0, 0]  # set first interface
+    else:
+        i0 = float(args.i0)
+
+    if args.iN == "intfN":
+        iN = float(x[-1, 0])  # set last interface
+    else:
+        iN = float(args.iN)
     N = args.num  # number of interfaces
-    order = 5 if args.order is None else args.order
     alpha = 0  # hyperparameter to penalize positive derivative of pcross, e.g. 50
 
     last_idx = np.where(x[:, 0] <= iN)[0][-1]
+    first_idx = np.where(x[:, 0] >= i0)[0][0]
 
-    y_fit = np.log(x[:last_idx, 1]) - np.log(x[last_idx, 1])  # y[-1]=0
-    x_fit = x[:last_idx, 0] / (x[last_idx, 0] - x[0, 0])  # x_fit in range (0,1)
+    y_fit = np.log(x[first_idx:last_idx, 1]) - np.log(x[last_idx, 1])  # y[-1]=0
+    x_fit = x[first_idx:last_idx, 0] / (x[last_idx, 0] - x[first_idx, 0])  # x_fit in range (0,1)
     shift = x_fit[0]
     x_fit -= shift
 
     # first points in pcross should match
     f0 = y_fit[0]
-
 
     def fnc(x, *p, f0=f0):
         """
@@ -92,9 +103,8 @@ def estimate_interfaces(arguments):
         idx = np.where(dy > 0)[0]
         return np.sqrt(np.sum((y - y_fit) ** 2)) + alpha * np.sum(dy[idx])
 
-
     # some initial guess of parameters
-    popt, pcov = curve_fit(fnc, x_fit, y_fit, p0=np.ones(order))
+    popt, pcov = curve_fit(fnc, x_fit, y_fit, p0=np.ones(args.order))
 
     # actual optimization with penalizing derivatives
     res = minimize(of, popt, args=(x_fit, y_fit, f0, alpha), method="Nelder-mead")
@@ -104,10 +114,16 @@ def estimate_interfaces(arguments):
 
     # transform back to original coordiantes
     y_plot = x[last_idx, 1] * np.exp(y_eval)
-    x_plot = x_eval * (x[last_idx, 0] - x[0, 0]) + x[0, 0]
+    x_plot = x_eval * (x[last_idx, 0] - x[first_idx, 0]) + x[first_idx, 0]
 
+    # divide by y_plot[0] here in case intf0 is not equal to default
+    # then shift back for plotting later
+    y0 = y_plot[0]
+    y_plot = y_plot/y0
     pcross = y_plot[-1]  # total crossing probability
+
     pt = np.exp(np.log(pcross) / (N - 1))  # local crossing probability
+    print(f"Plocal = {pt}")
 
     interfaces = [i0]  # first interface
     id_interfaces = [0]
@@ -125,7 +141,7 @@ def estimate_interfaces(arguments):
     if args.plot:
         f, a = plt.subplots(figsize=(8, 4))
         a.plot(x[:, 0], x[:, 1], marker="o", markersize=3)
-        a.plot(x_plot, y_plot)
+        a.plot(x_plot, y_plot*y0)
         for inter in interfaces:
             a.axvline(inter, c="k", lw=1)
         a.set(yscale="log")
--- inftools/initial_paths.py
+++ inftools/initial_paths.py
@@ -3,9 +3,7 @@ def generate_zero_paths(arguments):
     import os
     import pathlib
     import shutil
-    
     import numpy as np
-    
     from infretis.classes.engines.factory import create_engines
     from infretis.classes.orderparameter import create_orderparameters
     from infretis.classes.path import Path, paste_paths
@@ -39,16 +37,16 @@ def generate_zero_paths(arguments):
 
     # maximal length of initial paths
     maxlen = args.maxlen
-    
+
     # infretis parameters
     config = setup_config(args.toml)
     state = REPEX_state(config, minus=True)
-    
+
     # setup ensembles
     state.initiate_ensembles()
     state.engines = create_engines(config)
     create_orderparameters(state.engines, config)
-    
+
     # initial configuration to start from
     system0 = System()
     engine = state.engines[config["engine"]["engine"]]
@@ -58,11 +56,11 @@ def generate_zero_paths(arguments):
         {"wmdrun": config["dask"]["wmdrun"][0], "exe_dir": engine.exe_dir}
     )
     system0.set_pos((os.path.join(engine.input_path, initial_configuration), 0))
-    
+
     # empty paths we will fill forwards in time in [0-] and [0+]
     path0 = Path(maxlen=maxlen)
     path1 = Path(maxlen=maxlen)
-    
+
     # propagate forwards from the intiial configuration
     # note that one of these does not get integrated because
     # the initial phasepoint is either below or above interface 0
@@ -71,7 +69,7 @@ def generate_zero_paths(arguments):
     system0.set_pos((os.path.join(engine.input_path, initial_configuration), 0))
     print("Propagating in ensemble [0+]")
     status1, message1 = engine.propagate(path1, state.ensembles[1], system0)
-    
+
     # we did only one integration step in ensemble 0 because
     # we started above interface 0
     if path0.length == 1:
@@ -79,7 +77,7 @@ def generate_zero_paths(arguments):
         system0.set_pos((engine.dump_config(path1.phasepoints[-1].config), 0))
         path0 = Path(maxlen=maxlen)
         status0, message0 = engine.propagate(path0, state.ensembles[0], system0)
-    
+
     # or we did only one integration step in ensemble 1 because
     # we started below interface 0
     elif path1.length == 1:
@@ -87,25 +85,25 @@ def generate_zero_paths(arguments):
         system0.set_pos((engine.dump_config(path0.phasepoints[-1].config), 0))
         path1 = Path(maxlen=maxlen)
         status1, message1 = engine.propagate(path1, state.ensembles[1], system0)
-    
+
     else:
         raise ValueError("Something fishy!\
                 Path lengths in one of the ensembles != 1")
-    
+
     # backward paths
     path0r = Path(maxlen=maxlen)
     path1r = Path(maxlen=maxlen)
-    
+
     print("Propagating [0-] in reverse")
     status0, message0 = engine.propagate(
         path0r, state.ensembles[0], path0.phasepoints[0], reverse=True
     )
-    
+
     print("Propagating [0+] in reverse")
     status1, message1 = engine.propagate(
         path1r, state.ensembles[1], path1.phasepoints[0], reverse=True
     )
-    
+
     print("Done! Making load/ dir")
     # make load directories
     pathsf = [path0, path1]
