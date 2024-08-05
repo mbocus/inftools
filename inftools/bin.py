28fa86df8f3d7273bd820b5ba5e724ea06ab22ab
dz24 <daniel.t.zhang@ntnu.no>
2024-08-05 16:27:31 +0200

    update

--- inftools/bin.py
+++ inftools/bin.py
@@ -9,7 +9,7 @@ from inftools.analysis.wham import wham
 
 # define constants
 MOD_PATH = str(pathlib.Path(__file__).parent.resolve())
-FOLDERS = ["exercises", "tis_tools"]
+FOLDERS = ["exercises", "tistools"]
 MAPPER = get_mapper(FOLDERS, MOD_PATH)
 
 # add individual functions
