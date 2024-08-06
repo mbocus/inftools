a09276aa0eeedc5d9f9ed7088ff19309df5601c9
dz24 <daniel.t.zhang@ntnu.no>
2024-08-06 14:33:29 +0200

    add double well simdata

---
+++ test/test_tistools/test_concatenate.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+from inftools.tistools.concatenate import concatenate_xyz
+from infretis.classes.engines.cp2k import read_xyz_file
+
+def read_xyz(inp):
+    cnt = 0
+    with open(inp, 'r') as read:
+        for line in read:
+            if 'frame' in line:
+                cnt += 1
+    return cnt
+
+
+
+def test_concatenate(tmp_path):
+    """Check that we can modify the velocities with an engine,
+    and that they are not equal to zero."""
+    # folder we wil run from
+    folder = tmp_path / "temp/"
+    folder.mkdir()
+    pathpath= os.path.dirname(__file__) + "/../simdata_dw/load/210"
+    shutil.copytree(pathpath, str(folder) + "/path_xyz")
+    os.chdir(str(folder)+"/path_xyz/accepted/")
+
+    out = "merged.xyz"
+    concatenate_xyz(out=out, traj="../traj.txt")
+    cnt = read_xyz(out)
+
+    # check if merged file exist and is of length 90
+    assert os.path.isfile(out)
+    assert cnt == 133
+
