060494ac3eb048cda4c25df89af67917c436d279
dz24 <daniel.t.zhang@ntnu.no>
2024-10-23 17:12:53 +0900

    remove comments

--- test/test_tistools/test_infinit.py
+++ test/test_tistools/test_infinit.py
@@ -26,42 +26,8 @@ def test_infinit_1(tmp_path: PosixPath) -> None:
     shutil.copy(str(conf_dir), str(folder))
     os.chdir(folder)
 
-    print('banda', folder)
-    # assert False
-    success = os.system("inft infinit")
-
-    # Check if files exist and that p is same or growing
-    tomls = [f"infretis_{i}.toml" for i in range(2, 11)] + ["infretis.toml"]
-    assert os.path.isfile(tomls[0])
-    config0 = tomli.load(tomls[0])
-    assert "infinit" in config0
-    assert "p" in config0["infinit"]
-    for item in items[1:]:
-        assert os.path.isfile(item)
-        config = tomli.load(item)
-        assert "infinit" in config
-        assert "p" in config["infinit"]
-        assert len(config["infinit"]["p"]) >= len(config0["infinit"]["p"])
-
-
-@pytest.mark.heavy
-def test_infinit_2(tmp_path: PosixPath) -> None:
-    """Test infinit from existing sim"""
-    folder = tmp_path / "temp"
-    folder.mkdir()
-    basepath = PosixPath(__file__).parent
-    load_dir = (
-        basepath / "../../examples/turtlemd/double_well/load_copy"
-    ).resolve()
-    toml_dir = basepath / "data/infretis.toml"
-    conf_dir = basepath / "data/initial.xyz"
-    shutil.copy(str(toml_dir), str(folder))
-    shutil.copy(str(conf_dir), str(folder))
-    os.chdir(folder)
-
-    print('banda', folder)
-    # assert False
-    success = os.system("inft infinit")
+    # Run infinit
+    os.system("inft infinit")
 
     # Check if files exist and that p is same or growing
     tomls = [f"infretis_{i}.toml" for i in range(2, 11)] + ["infretis.toml"]
