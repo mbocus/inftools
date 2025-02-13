efbc6fb1c39d6b78c89c40e89a631c0ac23a7542
dz24 <rubidxx.dz@gmail.com>
2025-02-13 18:58:41 +0900

    comments

--- inftools/tistools/traj_conv.py
+++ inftools/tistools/traj_conv.py
@@ -27,16 +27,41 @@ def trajtxt_conv(
             files.append(file)
 
     # iterate over files
-    for file in files:
+    for file_nr, file in enumerate(files):
 
         # get first, last and direction of frames in file
         file_idx_s = int(data[data[:, 1] == file][0][2])
         file_idx_e = int(data[data[:, 1] == file][-1][2])
         it = int(data[data[:, 1] == file][0][-1])
 
+        # to account for overlap:
         # fill new data list
-        for idx in np.arange(file_idx_s*r, file_idx_e*r + it, it):
+        extra_f, extra_b = 0, 0
+        # usually first file trajectory contains all frames
+        if file_nr > 0:
+            # reverse,
+            if it == -1:
+                extra_b = 4
+            elif it == 1 and file_idx_s == 1:
+                extra_b = -4
+                extra_f = 4
+            # only one frame, traj usually contain at least two subcycles
+            elif file_idx_s == file_idx_e:
+                if it == 1:
+                    extra_f = 4
+                else:
+                    extra_b = 4
+
+        print(file)
+        print(np.arange(file_idx_s*r + extra_b, file_idx_e*r + it + extra_f, it), len(np.arange(file_idx_s*r + extra_b, file_idx_e*r + it + extra_f, it)))
+        print(file_idx_s, file_idx_e, it)
+        print(file_idx_s*r + extra_b, file_idx_e*r + it + extra_f, extra_f, extra_b)
+        print('')
+
+        for idx in np.arange(file_idx_s*r + extra_b, file_idx_e*r + it + extra_f, it):
             data_out.append([file.replace(".trr", ".xtc"), idx, it])
+
+    print("len", len(data_out))
 
     with open(o, "w") as write:
         write.write("# comment\n")
