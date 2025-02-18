91273b9f5ccfa47434c19d1781e5a46ec6d591c7
dz24 <rubidxx.dz@gmail.com>
2025-02-18 12:46:33 +0900

    lint

--- inftools/tistools/plot_msg.py
+++ inftools/tistools/plot_msg.py
@@ -1,22 +1,23 @@
-import argparse
 from typing import Annotated
+
 import typer
 
 
 def plot_msg(
     toml: Annotated[str, typer.Option("-toml")] = "infretis.toml",
-    single: Annotated[bool, typer.Option("-single")] = False,
-    ):
+):
     """Plot the order printed in the worker*/msg* files, so we
     can visualize the progress of the shooting moves.
 
     Require that the user be in the root sim folder.
 
-	TODO: color code last accepted WF subpath and the extension
-	phase."""
+        TODO: color code last accepted WF subpath and the extension
+        phase."""
     import os
-    import numpy as np
+
     import matplotlib.pyplot as plt
+    import numpy as np
+
     from inftools.misc.infinit_helper import read_toml
 
     intfs = []
@@ -37,12 +38,12 @@ def plot_msg(
 
     # Set grid size
     x_grid, y_grid = 1, 1
-    while x_grid*y_grid < workers:
-        x_grid +=1
-        if x_grid*y_grid >= workers:
+    while x_grid * y_grid < workers:
+        x_grid += 1
+        if x_grid * y_grid >= workers:
             break
-        y_grid +=1
-        if x_grid*y_grid >= workers:
+        y_grid += 1
+        if x_grid * y_grid >= workers:
             break
 
     fig, axes = plt.subplots(x_grid, y_grid)
@@ -56,7 +57,7 @@ def plot_msg(
     for i in range(workers):
         wfolder = f"worker{i}"
         if not os.path.isdir(wfolder):
-        	break
+            break
         idx = 0
         ens = -1
 
@@ -70,15 +71,42 @@ def plot_msg(
             try:
                 msg = np.loadtxt(wfolder + "/" + file)
                 if "trajB" in file:
-                    axes[i].plot(msg[:, 0] + idx, msg[::-1, 1], ls="--", color=f"C{i%8}")
-                    axes[i].scatter([msg[0, 0] + idx], [msg[-1, 1]], color=f"C{i%8}", marker="<", edgecolor='k', zorder=100)
-                    axes[i].scatter([msg[-1, 0] + idx], [msg[0, 1]], color=f"k", marker="x", zorder=100)
+                    axes[i].plot(
+                        msg[:, 0] + idx, msg[::-1, 1], ls="--", color=f"C{i%8}"
+                    )
+                    axes[i].scatter(
+                        [msg[0, 0] + idx],
+                        [msg[-1, 1]],
+                        color=f"C{i%8}",
+                        marker="<",
+                        edgecolor="k",
+                        zorder=100,
+                    )
+                    axes[i].scatter(
+                        [msg[-1, 0] + idx],
+                        [msg[0, 1]],
+                        color="k",
+                        marker="x",
+                        zorder=100,
+                    )
                 else:
-                    axes[i].plot(msg[:, 0] + idx -1, msg[:, 1], color=f"C{i%8}")
-                    axes[i].scatter([msg[-1, 0] + idx -1], [msg[-1, 1]], color=f"C{i%8}", marker=">", edgecolor='k')
+                    axes[i].plot(
+                        msg[:, 0] + idx - 1, msg[:, 1], color=f"C{i%8}"
+                    )
+                    axes[i].scatter(
+                        [msg[-1, 0] + idx - 1],
+                        [msg[-1, 1]],
+                        color=f"C{i%8}",
+                        marker=">",
+                        edgecolor="k",
+                    )
                 idx += len(msg[:, 0])
             except:
-                print("cannot plot", wfolder + "/" + file, "perhaps no op printed yet")
+                print(
+                    "cannot plot",
+                    wfolder + "/" + file,
+                    "perhaps no op printed yet",
+                )
 
         # plt ensemble intfs
         if intfs and ens > -1:
@@ -86,10 +114,10 @@ def plot_msg(
 
             axes[i].axhline(intfs[intf_idx], color=f"C{i%8}", alpha=0.2)
             axes[i].set_title(f"{ens:03}-worker{i}")
-            axes[i].axhline(intfs[0], color='k')
-            axes[i].axhline(intfs[-1], color='k')
+            axes[i].axhline(intfs[0], color="k")
+            axes[i].axhline(intfs[-1], color="k")
             if cap is not None:
-                axes[i].axhline(cap, color='r', ls="--")
+                axes[i].axhline(cap, color="r", ls="--")
 
     if workers > 0:
         plt.show()
